955e29129b8ffd1d6cea79623a4bdca1bb9b83df
何东升 <he77505357@163.com>
2021-06-22 15:50:09 +0800

    activity-01

---
+++ app/src/main/java/com/example/demo01/DisplayMessageActivity.java
@@ -0,0 +1,19 @@
+package com.example.demo01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        Intent intent = getIntent();
+        // Capture the layout's TextView and set the string as its text
+
+    }
+}
